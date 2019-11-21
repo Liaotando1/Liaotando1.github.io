9d90d0d69f0fdb43799c40117a96d9e62bc76a1e
= <=>
2019-11-21 15:11:44 +0700

    makanan & minuman

---
+++ precache-manifest.c7f87dea5d0d1a5077ca840fead0418d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "129b7df2417ade351a59",
+    "url": "/css/about.3b3c9ddb.css"
+  },
+  {
+    "revision": "4c05b131779fe9c39c49",
+    "url": "/css/chunk-vendors.ec4de8ca.css"
+  },
+  {
+    "revision": "d83943a55356e2cc803823603e835b82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "129b7df2417ade351a59",
+    "url": "/js/about.40e06f2b.js"
+  },
+  {
+    "revision": "ae903d2e1db9026f9d37",
+    "url": "/js/app.56872402.js"
+  },
+  {
+    "revision": "4c05b131779fe9c39c49",
+    "url": "/js/chunk-vendors.2ad0cc9e.js"
+  },
+  {
+    "revision": "9220e28cdbe46d8b27020616b32bead9",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
